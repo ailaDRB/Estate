bf72267fc44e7759ff6c9ff9b724ed086855817d
Dalia <bacaridalia@outlook.fr>
2022-06-22 15:27:22 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyComponent } from './property/property.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PropertyComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
