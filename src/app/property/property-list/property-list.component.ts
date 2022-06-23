fdcde34617631a7ed557fa355605f6c34708c8d9
Dalia <daliarbacari@gmail.com>
2022-06-23 14:44:42 +0100

    Middle

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -8,19 +8,20 @@ import { HousingService } from 'src/app/services/housing.service';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties: any;
+  properties: Array <any>;
 
   constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
     this.housingService.getAllProperties().subscribe(
       data=>{
-       this.properties=data
+       this.properties = data;
        console.log(data);
      }, error => {
+      console.log('httperror:');
        console.log(error);
      }
-    )
+    );
 
   }
 
