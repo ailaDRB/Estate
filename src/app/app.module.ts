3b77c3ef7664b797462273ec9b08e5b691847a1f
Dalia <daliarbacari@gmail.com>
2022-06-23 22:01:46 +0100

    inProgress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,9 @@ const appRoutes: Routes = [
   {path: 'rent-property', component: PropertyListComponent },
   {path: 'add-property', component: AddPropertyComponent },
   {path: 'property-detail/:id', component: PropertyDetailComponent },
+  {path: '**', component: PropertyListComponent },
 ]
+
 
 @NgModule({
   declarations: [
