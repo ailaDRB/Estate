d6e2c73bd7a8cb9190916ac5f3ab400fa284b469
Dalia <daliarbacari@gmail.com>
2022-07-01 10:25:18 +0100

    main

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -3,17 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators'
 import { Observable } from 'rxjs';
 import { IPropertyBase } from '../model/ipropertybase';
+import { Property } from '../model/property';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
+  baseUrl: string;
 
   addProperty(propertyId: number) {
   ;
   }
 
   constructor(private http: HttpClient) { }
+  getProperty(id: number) {
+    return this.http.get<Property>(this.baseUrl + '/property/detail/'+id.toString());
+}
 
   getAllProperties(SellRent?: number): Observable <IPropertyBase[]> {
     return this.http.get('data/properties.json').pipe(
